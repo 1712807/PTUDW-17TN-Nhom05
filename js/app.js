8d0b6d4740ad7cccfc1c824f969ae4c122e02fb0
Minh Thuy <minhthuy99cbler@gmail.com>
2020-06-01 14:48:52 +0700

    Update

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function Signup()
+{
+    var btn_signup = document.getElementById("btn-signup");
+    
+    btn_signup.style.visibility="hidden";
+
+    var btn_user = document.getElementById("user");
+
+    btn_user.style.visibility="visible";
+
+    
+}
